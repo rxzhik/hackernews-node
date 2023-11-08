c5993da78ea01a721ec0752ef524ba2809ec1527
rxzhik <rashikraj12@gmail.com>
2023-11-08 16:39:51 +1100

    Created the temporary frontend for the application

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,8 +1,22 @@
-// src/App.tsx
+import UserDisplay from "./components/UserDisplay"
+import { User } from "./types"
+
 function App() {
+
+  const users: User[] = [{
+    name: 'Prisma Fan',
+    messages: [{
+      body: 'Prisma rocks!!'
+    }, {
+      body: 'Did I mention I love Prisma?'
+    }]
+  }]
+
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      <h2 className="text-4xl text-yellow-500">Hello World!</h2>
+      {
+        users.map((user, i) => <UserDisplay user={user} key={i}/>)
+      }
     </div>
   )
 }
